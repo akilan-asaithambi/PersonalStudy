0ac53c14e263b067049ec417f1815ac2880640a7
akilan-asaithambi <akilanasaithambi07@gmail.com>
2024-04-03 12:29:51 +0530

    feat: initial commit

--- demopractice/Program.cs
+++ demopractice/Program.cs
@@ -1,5 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("new console created");
-Console.WriteLine("second console created");
-Console.WriteLine(" this is first github checking process first i need to created one console project after that i connect to the git repository ");
+﻿/*// See https://aka.ms/new-console-template for more information
+Console.WriteLine("this is another branch");*/
+public class Program
+{
+    public void firstMethod()
+    {
+        Console.WriteLine("this first method");
+    }
+}
