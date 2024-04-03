f8810858376ae5cb518b92dab15eff5a87a28fd1
akilan-asaithambi <165481607+akilan-asaithambi@users.noreply.github.com>
2024-04-03 11:57:42 +0530

    fix: remove multiple console.

--- demopractice/Program.cs
+++ demopractice/Program.cs
@@ -2,4 +2,3 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("new console created");
 Console.WriteLine("second console created");
-Console.WriteLine(" this is first github checking process first i need to created one console project after that i connect to the git repository ");
