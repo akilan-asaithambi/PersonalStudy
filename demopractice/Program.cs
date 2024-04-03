d2f476bdd515d1fb539f910847796b727915f676
akilan-asaithambi <akilanasaithambi07@gmail.com>
2024-04-03 12:06:46 +0530

    Revert "fix: remove multiple console."
    This reverts commit f8810858376ae5cb518b92dab15eff5a87a28fd1.

--- demopractice/Program.cs
+++ demopractice/Program.cs
@@ -2,3 +2,4 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("new console created");
 Console.WriteLine("second console created");
+Console.WriteLine(" this is first github checking process first i need to created one console project after that i connect to the git repository ");
